60c7688d8aad13762c430bc674cfe97b03a8b144
Eliesser Freites <freiteseliesser@hotmail.com>
2024-04-19 19:50:21 +0200

    update

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,14 +1,15 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsSessionDto } from './dto/payments-session.dto';
 import { Request, Response } from 'express';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Post('create-payment-session')
-  createPaymentSession(@Body() paymentsSessionDto: PaymentsSessionDto) {
+  @EventPattern('create.payment.session')
+  createPaymentSession(@Payload() paymentsSessionDto: PaymentsSessionDto) {
     return this.paymentsService.createPaymentSession(paymentsSessionDto);
   }
 
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable } from '@nestjs/common';
+import { NATS_SERVICE, envs } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentsSessionDto } from './dto/payments-session.dto';
 import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   async createPaymentSession({ currency, items, orderId }: PaymentsSessionDto) {
     const line_items = items.map(({ name, price, quantity }) => ({
@@ -20,7 +23,7 @@ export class PaymentsService {
       quantity,
     }));
 
-    return await this.stripe.checkout.sessions.create({
+    const paymentSession = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
         metadata: {
           orderId,
@@ -31,6 +34,12 @@ export class PaymentsService {
       success_url: envs.stripeSuccessUrl,
       cancel_url: envs.stripeCancelUrl,
     });
+
+    return {
+      cancelUrl: paymentSession.cancel_url,
+      successUrl: paymentSession.success_url,
+      url: paymentSession.url,
+    };
   }
 
   async stripeWebhook(req: Request, res: Response) {
@@ -53,7 +62,13 @@ export class PaymentsService {
     if (event.type === 'charge.succeeded') {
       const chargeSucceeded = event.data.object;
 
-      console.log(chargeSucceeded.metadata.orderId);
+      const payload = {
+        stripePaymentId: chargeSucceeded.id,
+        orderId: chargeSucceeded.metadata.orderId,
+        receiptUrl: chargeSucceeded.receipt_url,
+      };
+
+      this.client.emit('payment.succeeded', payload);
     }
 
     return res.status(200).json({ sig });
